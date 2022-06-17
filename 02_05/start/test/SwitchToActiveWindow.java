38441ecc03df81ec6c09101521716433599e4a89
SavvasNicoleCandiotti <nicole.candiotti@savvas.com>
2022-06-17 09:34:19 -0400

    first commit

--- 02_05/start/test/SwitchToActiveWindow.java
+++ 02_05/start/test/SwitchToActiveWindow.java
@@ -6,12 +6,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SwitchToActiveWindow {
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "/Users/meaghanlewis/Downloads/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/opt/homebrew/bin/chromedriver");
 
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://formy-project.herokuapp.com/switch-window");
 
+        WebElement newTab = driver.findElement(By.id("new-tab-button"));
+        newTab.click();
+
+//        getWindowHandle gets handle of original window
+        String originalHandle = driver.getWindowHandle();
+
+        for(String handle1: driver.getWindowHandles()){
+            driver.switchTo().window(handle1);
+        }
+//        switches back to original window 
+        driver.switchTo().window(originalHandle);
         driver.quit();
     }
 }
